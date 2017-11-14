e99f49cfacdb4a7fb95aab4b5916ef9d6cee4fc8
Gonzalo Sanchez Pla <gonzalo@cornac.es>
2017-11-14 19:20:01 +0100

    Included Iterators, filters.

--- 04 - Language Features/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ 04 - Language Features/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -1,29 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using LanguageFeatures.Models;
+using System;
+using System.Linq;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController:Controller
     {
+        bool FilterByPrice(Product p)
+        {
+            return (p?.Price ?? 0) >= 20;
+        }
+
         public ViewResult Index()
         {
-            object[] data = new object[] { 275M, 29.95M,
-            "apple", "orange", 100, 10 };
-            decimal total = 0;
-            for (int i = 0; i < data.Length; i++)
-            {
-                switch (data[i])
-                {
-                    case decimal decimalValue:
-                        total += decimalValue;
-                        break;
-                    case int intValue when intValue > 50:
-                        total += intValue;
-                        break;
-                }
-            }
-            return View("Index", new string[] { $"Total: {total:C2}" });
+            Product[] productArray = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+
+            Func<Product, bool> nameFilter = delegate (Product prod) {
+                return prod?.Name?[0] == 'S';
+            };
+
+            decimal priceFilterTotal = productArray
+                .Filter(p => (p?.Price ?? 0) >= 20)
+                .TotalPrices();
+            decimal nameFilterTotal = productArray
+                .Filter(p => p?.Name?[0] == 'S')
+                .TotalPrices();
+
+            return View("Index", new string[] 
+            {$"Price Total: {priceFilterTotal:C2}",
+                $"Name Total: {nameFilterTotal:C2}" });
         }
     }
 }
--- 04 - Language Features/LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ 04 - Language Features/LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -35,6 +35,8 @@
 
             return new Product[] { kayak, lifejacket, null };
         }
+
+        
     }
 }
 
